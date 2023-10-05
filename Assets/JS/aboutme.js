295e69c94f4825d976072b19f2fdabc8b3a7120d
Cody Schwengler01~ <cschwengler@outlook.com>
2023-10-05 16:04:38 -0700

    created js for button on about me page

---
+++ Assets/JS/aboutme.js
@@ -0,0 +1,8 @@
+//when button is clicked it transports the user to the projects page//
+let button = document.getElementById('button')
+
+function Projects(){
+    window.location.href = 'projects.html';
+}
+
+button.addEventListener('click', Projects);
